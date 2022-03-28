22a69586bf76f8e4936389f92756f13ef830b7bc
indhu0326 <101918911+indhu0326@users.noreply.github.com>
2022-03-28 15:27:16 +0530

    Add files via upload

---
+++ program1.py
@@ -0,0 +1,13 @@
+x = input("Type a number: ")
+y = input("Type another number: ")
+
+sum = int(x) + int(y)
+
+print("The sum is: ", sum)
+
+x = input("Type a number: ")
+y = input("Type another number: ")
+
+sub = int(x) - int(y)
+
+print("The sub is: ", sum)
